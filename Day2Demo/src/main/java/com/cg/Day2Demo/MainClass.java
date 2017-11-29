9e589edd7c5a8b11e01c91723589aea5fcd3d46e
lnc <lnc@DIN76000838.corp.capgemini.com>
2017-11-29 10:26:24 +0530

    commit all

---
+++ Day2Demo/src/main/java/com/cg/Day2Demo/MainClass.java
@@ -0,0 +1,13 @@
+package com.cg.Day2Demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		OrderClass obj= new OrderClass();
+		obj.display();
+	//gretings();
+		
+
+	}
+
+}
---
+++ Day2Demo/src/main/java/com/cg/Day2Demo/OrderClass.java
@@ -0,0 +1,11 @@
+package com.cg.Day2Demo;
+
+public class OrderClass {
+	int[] arr={1,23,45,-98,76};
+	public void display()
+	{
+		for(int i=0;i<5;i++);
+		System.out.println("");
+	}
+
+}
